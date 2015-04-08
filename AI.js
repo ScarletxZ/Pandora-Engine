b3505218872b6246cfcb679a760ff3294c3c2004
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-04-08 15:08:02 +0800

    Update AI.js

--- AI.js
+++ AI.js
@@ -9,23 +9,21 @@
 var description = "Hello, meet Pandora, a mascot for Pandora Engine. Pandora will help you code in JavaScript, every single code
 and every single syntax errors you wrote will results the warning from Pandora itself.";
 
-var switch = 'enable'; /* set to 'enable' if you like to use the audio to make Pandora talk with a sound, not only the text,
+var audioSwitch = 'enable'; /* set to 'enable' if you like to use the audio to make Pandora talk with a sound, not only the text,
 set to disable for otherwise */
 
 /* ======================== END ============================== */
 
 /* ================= Warning : Don't touch UNLESS IF YOU KNOW WHAT YOU ARE DOING! =============== */
 
-if(switch.toLowerCase() == 'disable') {
- window.activateAudio === false;
-}
-
-if(switch.toLowerCase() == 'enable') {
- window.activateAudio === true;
-}
-/* FYI : to avoid case-sensitives, we'll change it into lower-case so it'll becomes lower case */
-
+if(audioSwitch.toLowerCase() == 'disable') { window.activateAudio === false; }
+if(audioSwitch.toLowerCase() == 'enable') { window.activateAudio === false; }
 console.log(description);
-/* FYI : we'll log the description of the AI */
+function activateAudio() { 
+ audio1 = new Audio('audio_link_1');
+ audio1.play;
+ audio2 = new Audio('audio_link_2');
+ audio2.play;
+}
 
-/* more code here */
+/* will edit later if found some resources */
